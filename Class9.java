4c3b0cef54931ad737b39fee6af0b53e2641ed18
chandrasekhar009 <31537403+chandrasekhar009@users.noreply.github.com>
2017-10-11 16:33:19 +0530

    commit

---
+++ Class9.java
@@ -0,0 +1,16 @@
+package project8;
+
+import java.util.Scanner;
+
+public class Class9 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter username");
+		String u=sc.nextLine();
+		System.out.println("enter password");
+		String p=sc.nextLine();
+	
+	}
+}
